b1743b5764b75c22908a5462de7ae15ed90511df
sjbloom <scott.bloomfield@healthcatalyst.com>
2017-05-24 14:52:32 -0600

    Add all content

---
+++ PowerShell/sqlServerConnection.ps1
@@ -0,0 +1,12 @@
+﻿#Connect to sql server
+$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = "Server=HC2012\HCLAPTOPSB;Database=EdwAdmin;Integrated Security=True" 
+$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+$SqlCmd.Connection = $SqlConnection
+$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+$DataSet = New-Object System.Data.DataSet
+$SqlCmd.CommandText = "EXEC EDWADMIN.ClientAdmin.MDMActivityUpdate applicationstatus,$($SucessIndicator)"
+$SqlAdapter.SelectCommand = $SqlCmd
+$SqlAdapter.Fill($DataSet) #execute the sql
+$SqlConnection.Close()
+#$DataSet.Tables[0]
